8d0cd48886802da4e04757fddcd90a30fcf3583a
Aluno <joaopedroangeloni@gmail.com>
2023-02-15 10:57:17 -0300

    BCD

--- BCD/Aula05/script.sql
+++ BCD/Aula05/script.sql
@@ -3,24 +3,57 @@ CREATE DATABASE escola CHARSET=UTF8 COLLATE utf8_general_ci;
 USE escola;
 
 CREATE TABLE professores(
-    id INT not null PRIMARY KEY, 
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     nome varchar(100) not null,
-    especialização varchar(50) not null
+    especialidade varchar(50) not null
 );
 
 CREATE TABLE alunos(
     ra varchar(15) not null PRIMARY KEY,
     nome varchar(50) not null,
-    nascto date not null
+    nascto date not null,
+    id_turma INTEGER NOT NULL
 );
 
 CREATE TABLE turmas(
-    id INT not null PRIMARY KEY,
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome varchar(50) not null,
     sigla varchar(4) not null
 );
 
-SHOW tables;
-DESCRIBE professores;
-DESCRIBE alunos;
-DESCRIBE turmas;
+ALTER TABLE alunos ADD FOREIGN KEY (id_turma) REFERENCES turmas(id);
+
+CREATE TABLE leciona(
+    id_prof INTEGER NOT NULL,
+    id_turma INTEGER NOT NULL,
+    FOREIGN KEY (id_prof) REFERENCES professores(id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_turma) REFERENCES turmas(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO professores VALUES
+(default, "Reenye", "Mobile"),
+(default, "Wellington", "Banco de Dados"),
+(default, "Ivo", "Matemática");
+
+INSERT INTO turmas VALUES
+(default, "Desenvolvimento de Sistemas", "2DES"),
+(default, "Desenvolvimento de Sistemas", "3DES"),
+(default, "Eletrônica", "1EA"),
+(default, "Eletrônica", "3EA");
+
+INSERT INTO alunos VALUES
+('ra01', "Camacho", "2000-04-02", 1),
+('ra02', "Karen", "2005-05-12", 1),
+('ra03', "Cyro", "1984-08-13", 2),
+('ra04', "Beatriz", "2002-08-05",2),
+('ra05', "Alexandre", "2001-12-12",3),
+('ra06', "Alexandre", "2001-12-12",3);
+
+INSERT INTO leciona VALUES
+(1,1),
+(1,2),
+(2,1),
+(2,2),
+(3,3);
