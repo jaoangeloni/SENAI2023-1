b584f042a149cd678ea59551cf8cc471962e56b7
Aluno <joaopedroangeloni@gmail.com>
2023-03-07 08:46:24 -0300

    PROJ

--- PROJ/imobiliaria/back/src/controllers/imovel.controller.js
+++ PROJ/imobiliaria/back/src/controllers/imovel.controller.js
@@ -27,12 +27,43 @@ const buscar = (req, res) => {
 }
 
 const adicionar = (req,res) => {
-    const {corretor_id, codigo, endereco, valor_venda, valor_aluguel, status_id} = req.body;
+    let {corretor_id, codigo, endereco, valor_venda, valor_aluguel} = req.body;
 
-    let query = `INSERT INTO imoveis VALUES ("",${corretor_id},${codigo},${endereco},${valor_venda},${valor_aluguel},${status_id})`;
+    valor_venda = (valor_venda != undefined) ? valor_venda : 0;
+    valor_aluguel = (valor_aluguel != undefined) ? valor_aluguel : 0;
+
+    if((valor_venda == 0) && (valor_aluga == 0)){
+        res.status(400).json({"msg" : "Necessário inserir um valor de venda ou aluguel"});
+    }else{
+        let query = `INSERT INTO imoveis VALUES ("",${corretor_id}, "${codigo}", "${endereco}",${valor_venda},${valor_aluguel},1)`;
+        
+        con.query(query, (err, response) =>{
+            if(err == undefined){
+                res.status(200).json(response).end();
+            }else{
+                res.status(400).json(err).end();
+            }
+        });
+    }
+}
+
+const alterarStatus = (req, res) => {
+    const {codigo, status} = req.params;
+
+    let query = `UPDATE imoveis SET status_id = ${status} WHERE codigo = "${codigo}`;
+
+    con.query(query, (err,response) => {
+        if(err == undefined) {
+            res.status(200).json(response).end();
+        }else{
+            res.status(400).json(err).end();
+        }
+    })
 }
 
 module.exports = {
     listar,
-    buscar
+    buscar,
+    adicionar,
+    alterarStatus
 }
--- PROJ/imobiliaria/back/src/routes/imovel.route.js
+++ PROJ/imobiliaria/back/src/routes/imovel.route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.get("/imoveis",Imoveis.listar);
 router.get("/imoveis/:info", Imoveis.buscar);
+router.post("/imovel", Imoveis.adicionar);
+router.put("/imovel/:codigo/:statis", Imoveis.alterarStatus);
 
 module.exports = router;        
