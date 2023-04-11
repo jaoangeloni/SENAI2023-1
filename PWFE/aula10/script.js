7612949f6ba6c69b6fe644ec22f029be57509ab6
Aluno <joaopedroangeloni@gmail.com>
2023-04-11 08:43:12 -0300

    PWFE

--- PWFE/aula10/script.js
+++ PWFE/aula10/script.js
@@ -3,32 +3,35 @@ const input = document.querySelector("#input")
 
 function valorInput() {
     let cidade = input.value
-    api.get('/current.json?key=aadf26231a1e492fb28115659231004&q=' + cidade)
+    api.get('/current.json?key=aadf26231a1e492fb28115659231004&q=' + cidade + '&lang=pt')
       .then(resp => {
-          informacoes(resp)
-          function informacoes () {
+              let icon = document.querySelector("#icon")
               let nomeCidade = document.querySelector("#nomeCidade")
               let nomeEstado = document.querySelector("#nomeEstado")
-              let nomePais = document.querySelector("#nomePais")
+              let condition = document.querySelector("#condition")
+              let localTime = document.querySelector("#localTime")
+              
+              icon.src = resp.data.current.condition.icon
+              nomeCidade.innerHTML = resp.data.location.name
+              nomeEstadoPais.innerHTML =  resp.data.location.region + ",    " + "<b>" + resp.data.location.country + "</b>"
+              condition.innerHTML = "<b>" + resp.data.current.condition.text + "</b>"
+              localTime.innerHTML = ",    " + new Date(resp.data.location.localtime).toLocaleDateString()
+              
               let tC = document.querySelector("#tC")
               let tF = document.querySelector("#tF")
-              let localTime = document.querySelector("#localTime")
-              let icon = document.querySelector("#icon")
               let wind = document.querySelector("#wind")
               let humidity = document.querySelector("#humidity")
               let uv = document.querySelector("#uv")
-
-              icon.src = resp.data.current.condition.icon
-              nomeCidade.innerHTML = resp.data.location.name
-              nomeEstado.innerHTML = resp.data.location.region + ", "
-              nomePais.innerHTML = resp.data.location.country
-              tC.innerHTML = resp.data.current.temp_c + " ° Celsius"
-              tF.innerHTML = resp.data.current.temp_f + " ° Fahrenheit"
-              wind.innerHTML = "Ventos de " + resp.data.current.wind_kph + " Km/H"
-              humidity.innerHTML = "Umidade " + resp.data.current.humidity + "%"
-              uv.innerHTML = "Índice UV " + resp.data.current.wind_kph + "%"
-              localTime.innerHTML = resp.data.location.localtime
-            }   
+              let precip_mm = document.querySelector("#precip_mm")
+              let cloudy = document.querySelector("#cloudy")
+              
+              tC.innerHTML = "<b>" + resp.data.current.temp_c + "</b> ° Celsius"
+              tF.innerHTML = "<b>" + resp.data.current.temp_f + "</b> ° Fahrenheit"
+              wind.innerHTML = "Ventos de <b>" + resp.data.current.wind_kph + "</b> Km/H"
+              humidity.innerHTML = "Umidade <b>" + resp.data.current.humidity + "%</b>"
+              uv.innerHTML = "Índice UV <b>" + resp.data.current.wind_kph + "%</b>"
+              precip_mm.innerHTML = "Precipitação de <b>" + resp.data.current.precip_mm + "mm</b>"
+              cloudy.innerHTML = "Nebulosidade de " + "<b>" + resp.data.current.cloud + "%</b>"
         });
 }
 
