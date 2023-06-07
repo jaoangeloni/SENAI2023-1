84fd2c96451f58cd864e8d21c517eba055bfa3e8
Aluno <joaopedroangeloni@gmail.com>
2023-06-07 10:07:14 -0300

    PWBE

--- PWBE/aula17/front/script.js
+++ PWBE/aula17/front/script.js
@@ -1,71 +1,30 @@
-const uri = 'http://localhost:3000/'
+const uri = 'http://localhost:3000'
 const email = document.querySelector("#email")
 const senha = document.querySelector("#senha")
-const cadastro = document.querySelector('#cadastro')
-// fetch(uri + '/listar', { method: 'GET' })
-//     .then(resp => resp.json())
-//     .catch(err => console.error(err));
 
-const login = [
-        {
-        "id": 1,
-        "cpf": 44741604870,
-        "nome": "Carlin Ruindade Pura",
-        "email": "carlin@ruindade.com",
-        "senha": "maldade123",
-        "nascimento": new Date(16-03-2005).toLocaleDateString(),
-        "endereço":[
-            {
-                "cep": "13417052",
-                "numero": "12",
-                "complemento": ""
-            },
-        ],
-        "telefone":[
-            {
-                "telefone1": 1929319238,
-                "telefone2": 1823182123,
-                "telefone3": 1923129391
-            }
-        ],
-        "img": "https://avowhcbwko.cloudimg.io/v7/https://empreender.nyc3.digitaloceanspaces.com/grupowhats/9c57f804fafea384fe28095b89db683a?w=400&org_if_sml=1"
-    }
-]
-
-cadastro.addEventListener('submit', e => {
-    e.preventDefault();  
-    const body = {
+function autenticar() {
+    let data = {
         "email": email.value,
-        "senha": senha.value,
+        "senha": senha.value
     }
 
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-    };
+    let options = {
+        "method": "POST",
+        "headers": {
+            "Content-Type":"application/json"
+        },
+        "body": JSON.stringify(data)
+    }
 
-    options.body = JSON.stringify(body)
-
-    fetch(uri + 'login', options)
-        .then(resp => {
-            console.log(resp.data)
-        })
-})
-
-// const img = document.querySelector("#img")
-// img.src = login[0].img;
-
-// const pId = document.querySelector("#pid");
-// pId.innerHTML = `<b>ID</b>: ${login[0].id}`;
-
-// const pCpf = document.querySelector("#pcpf");
-// pCpf.innerHTML = `<b>CPF</b>: ${login[0].cpf}`;
-
-// const pNome = document.querySelector("#pnome");
-// pNome.innerHTML = `<b>Nome</b>: ${login[0].nome}`;
-
-// const pEmail = document.querySelector("#pemail");
-// pEmail.innerHTML = `<b>Email</b>: ${login[0].email}`;
-
-// const pNascimento = document.querySelector("#pnascimento");
-// pNascimento.innerHTML = `<b>Nascimento</b>: ${login[0].nascimento}`;
+    fetch(uri + '/login', options)
+    .then(resp => resp.json())
+    .then(resp => {
+        if(resp.length != 0){
+            window.localStorage.setItem('Dados', JSON.stringify(resp))
+            window.location.href= "./home.html";
+        }
+        else{
+            alert("Usuário não encontrado")
+        }
+    })
+}
